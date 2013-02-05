a40d3bc606d657044514608fa7ebce2b2220e202
Carlos Heitor Lain <atendimento@virtuati.com.br>
2013-02-05 01:51:08 -0200

    /src/char_sql renomeado para char /src/char renomado para char_desabilitado

--- src/char_desabilitado/inter.h
+++ src/char_desabilitado/inter.h
@@ -1,17 +1,16 @@
 // Copyright (c) Athena Dev Teams - Licensed under GNU GPL
 // For more information, see LICENCE in the main folder
 
-#ifndef _INTER_SQL_H_
-#define _INTER_SQL_H_
+#ifndef _INTER_H_
+#define _INTER_H_
 
 struct accreg;
-#include "../common/sql.h"
 
-int inter_init_sql(const char *file);
+int inter_init_txt(const char *file);
 void inter_final(void);
+int inter_save(void);
 int inter_parse_frommap(int fd);
 int inter_mapif_init(int fd);
-int mapif_send_gmaccounts(void);
 int mapif_disconnectplayer(int fd, int account_id, int char_id, int reason);
 
 int inter_log(char *fmt,...);
@@ -19,12 +18,10 @@ int inter_log(char *fmt,...);
 #define inter_cfgName "conf/inter_athena.conf"
 
 extern unsigned int party_share_level;
-
-extern Sql* sql_handle;
-extern Sql* lsql_handle;
-
 extern char main_chat_nick[16];
 
-int inter_accreg_tosql(int account_id, int char_id, struct accreg *reg, int type);
+//For TXT->SQL conversion
+extern char accreg_txt[];
+int inter_accreg_fromstr(const char *str, struct accreg *reg);
 
-#endif /* _INTER_SQL_H_ */
+#endif /* _INTER_H_ */
